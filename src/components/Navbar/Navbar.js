c03e08a33dbea94fec8f3a3868b4ccccca0cbefa
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2025-04-23 10:55:56 +0200

    updated Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,103 +11,39 @@ export default function Navbar() {
 
   const isActive = (path) => location.pathname === path;
 
+  const createButton = (path, text) => {
+    return (
+      <Button
+        sx={{
+          borderRadius: 0,
+          backgroundColor: "#3b3b3b",
+          color: isActive(path) ? "#ff3c38" : "white",
+          fontWeight: isActive(path) ? "bold" : "normal",
+          textTransform: "none",
+          borderBottom: isActive(path) ? "2px solid #ff3c38" : "none",
+          "&:hover": {
+            backgroundColor: "#4a4a4a",
+          },
+        }}
+      >
+        {text}
+      </Button>
+    );
+  };
+
   return (
     <Box sx={{ flexGrow: 1, width: "100%" }}>
       <AppBar id="Navbar" position="fixed">
         <Toolbar>
-          <Link to="/">
-            <Button
-              sx={{
-                borderRadius: 0,
-                backgroundColor: "#3b3b3b",
-                color: isActive("/") ? "#ff3c38" : "white",
-                fontWeight: isActive("/") ? "bold" : "normal",
-                textTransform: "none",
-                borderBottom: isActive("/") ? "2px solid #ff3c38" : "none",
-                "&:hover": {
-                  backgroundColor: "#4a4a4a",
-                },
-              }}
-            >
-              Home
-            </Button>
-          </Link>
+          <Link to="/">{createButton("/", "Home")}</Link>
 
-          <Link to="/kontakt">
-            <Button
-              sx={{
-                borderRadius: 0,
-                backgroundColor: "#3b3b3b",
-                color: isActive("/kontakt") ? "#ff3c38" : "white",
-                fontWeight: isActive("/kontakt") ? "bold" : "normal",
-                textTransform: "none",
-                borderBottom: isActive("/kontakt")
-                  ? "2px solid #ff3c38"
-                  : "none",
-                "&:hover": {
-                  backgroundColor: "#4a4a4a",
-                },
-              }}
-            >
-              Kontakt
-            </Button>
-          </Link>
+          <Link to="/kontakt">{createButton("/kontakt", "Kontakt")}</Link>
 
-          <Link to="/about">
-            <Button
-              sx={{
-                borderRadius: 0,
-                backgroundColor: "#3b3b3b",
-                color: isActive("/about") ? "#ff3c38" : "white",
-                fontWeight: isActive("/about") ? "bold" : "normal",
-                textTransform: "none",
-                borderBottom: isActive("/about") ? "2px solid #ff3c38" : "none",
-                "&:hover": {
-                  backgroundColor: "#4a4a4a",
-                },
-              }}
-            >
-              Über Uns
-            </Button>
-          </Link>
+          <Link to="/about">{createButton("/about", "Über Uns")}</Link>
 
-          <Link to="/faq">
-            <Button
-              sx={{
-                borderRadius: 0,
-                backgroundColor: "#3b3b3b",
-                color: isActive("/faq") ? "#ff3c38" : "white",
-                fontWeight: isActive("/faq") ? "bold" : "normal",
-                textTransform: "none",
-                borderBottom: isActive("/faq") ? "2px solid #ff3c38" : "none",
-                "&:hover": {
-                  backgroundColor: "#4a4a4a",
-                },
-              }}
-            >
-              FAQ
-            </Button>
-          </Link>
+          <Link to="/faq">{createButton("/faq", "FAQ")}</Link>
 
-          <Link to="/impressum">
-            <Button
-              sx={{
-                borderRadius: 0,
-                backgroundColor: "#3b3b3b",
-                color: isActive("/impressum") ? "#ff3c38" : "white",
-                fontWeight: isActive("/impressum") ? "bold" : "normal",
-                textTransform: "none",
-                borderBottom: isActive("/impressum")
-                  ? "2px solid #ff3c38"
-                  : "none",
-                "&:hover": {
-                  backgroundColor: "#4a4a4a",
-                },
-              }}
-            >
-              Impressum
-            </Button>
-          </Link>
+          <Link to="/impressum">{createButton("/impressum", "Impressum")}</Link>
         </Toolbar>
       </AppBar>
     </Box>
