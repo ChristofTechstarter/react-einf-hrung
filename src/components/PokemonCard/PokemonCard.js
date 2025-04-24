6d24913e761f880240e58931dbfd11ac4f9f3262
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2025-04-24 10:15:50 +0200

    updated PokemonCard.js

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -1,22 +1,18 @@
 import React from "react";
 import "./PokemonCard.css";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function PokemonCard(props) {
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate(`/pokemon/${props.id}`);
-  };
-
   return (
-    <div className="container" onClick={handleClick}>
-      <img src={props.image} alt="Pokemon" width={"200px"}></img>
-      <h2>{props.Name}</h2>
-      <p>Gewicht: {props.Gewicht}</p>
-      <p>Größe: {props.Größe}</p>
-      <p>Typ: {props.Typ}</p>
-    </div>
+    <Link to={`/pokemon/${props.id}`} style={{ textDecoration: "none" }}>
+      <div className="container">
+        <img src={props.image} alt="Pokemon" width={"200px"}></img>
+        <h2>{props.Name}</h2>
+        <p>Gewicht: {props.Gewicht}</p>
+        <p>Größe: {props.Größe}</p>
+        <p>Typ: {props.Typ}</p>
+      </div>
+    </Link>
   );
 }
 
